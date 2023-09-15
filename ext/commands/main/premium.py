11b5dd427c4a62232b18e7798c5ff3f4c7a1bd58
Sector0880 <revampcroot0880@gmail.com>
2023-09-15 10:05:33 +0300

    up

--- ext/commands/main/premium.py
+++ ext/commands/main/premium.py
@@ -20,6 +20,8 @@ class Premium(commands.Cog):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 
+	"""
+	
 	# запись проверок
 	@commands.hybrid_command(
 		name = "delete_premium_allservers",
@@ -71,6 +73,8 @@ class Premium(commands.Cog):
 			user_dmchannel = self.bot.get_user(guilds_config_data[str(guild.id)]["overview"]["owner-id"])
 			await user_dmchannel.send(f'премиум подписка на сервере `{guilds_config_data[str(guild.id)]["overview"]["guild_name"]}` закончилась')
 		await ctx.send(f"Успешно.")
+
+		
 	
 	# запись проверок
 	@commands.hybrid_command(
@@ -105,30 +109,6 @@ class Premium(commands.Cog):
 
 			await ctx.send("Успешно")
 		else: await ctx.send("Истории приобретения премиум-подписки нету")
-
-	# запись проверок
-	@commands.hybrid_command(
-		name = "check_premium",
-		description = "Проверить премиум-статус у сервера (показывает данные с базы данных)",
-		aliases = ["chpr", "pr5"],
-		with_app_command = True
-	)
-	async def check_premium(self, ctx):
-		# если сервер заблокирован то staff игнорируют это ограничение
-		if ctx.author.id not in staff_staffList_SpecialPerms() and not guild_bot_output(ctx): return await botFunctions.bot_output_blocked(ctx)
-		# команда работает только для staff с специальными правами (список staffList_SpecialPerms)
-		if ctx.author.id not in staff_staffList_SpecialPerms(): return await botFunctions.command_for_staff(ctx)
-
-		# open db
-		with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
-		privileges_0 = guilds_config_data[str(ctx.guild.id)]["additional-features"]["privileges"][0]  # path to privileges in server
-
-		#await ctx.send(f'```json\n{json.dumps(guilds_config_data[str(ctx.guild.id)]["overview"], ensure_ascii = False, indent = 4)}\n```')
-		await ctx.send(f'```ansi\n\u001b[{0};{32}m{json.dumps(privileges_0, ensure_ascii = False, indent = 4)}\u001b[0m\n```')
-		if guild_premium(ctx):
-			await ctx.send(f'Осталось: `{str(datetime.fromisoformat(guild_premium_time_end(ctx)) - datetime.now())[:-7]}`')
-		else:
-			await ctx.send("Премиум отсутствует")
 	
 	# запись проверок
 	@commands.hybrid_command(
@@ -220,16 +200,44 @@ class Premium(commands.Cog):
 		else:
 			return await interaction.response.send_message("Команда не найдена.", ephemeral = True)
 		await interaction.response.send_message(embed = emb, ephemeral = True)
+	
+		"""
+
+	@app_commands.command(
+		name = "check_premium",
+		description = "Проверить премиум-статус у сервера (показывает данные с базы данных)"
+	)
+	@botFunctions.check_command_permissions()
+	async def check_premium(self, interaction: discord.Interaction):
+		# open db
+		with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
+		privileges_0 = guilds_config_data[str(interaction.guild.id)]["additional-features"]["privileges"][0]  # path to privileges in server
+
+		emb = discord.Embed(
+			title = f'Премиум на сервере: {guilds_config_data[str(interaction.guild.id)]["overview"]["guild-name"]}',
+			color = 0xFFD700 if guild_premium(interaction) else 0x2b2d31
+		)
+		emb.add_field(
+			name = "Статус",
+			value= f'Осталось: `{str(datetime.fromisoformat(guild_premium_time_end(interaction)) - datetime.now())[:-7]}`' if guild_premium(interaction) else "Премиум отсутствует"
+		)
+		"""
+		emb.add_field(
+			name = "База данных",
+			value = f'```ansi\n\u001b[0;33m{json.dumps(privileges_0, ensure_ascii = False, indent = 4)}\u001b[0m```',
+			inline=False
+		)
+		"""
+		await interaction.response.send_message(embed = emb, ephemeral = True)
 
 
 	# запись проверок
 	# проверка окончания премиума
 	@tasks.loop(seconds = bot_tasks_loop_premium_check_premiumtime())
 	async def premium_check_premiumtime(self):
-		for guild in self.bot.guilds:
-			# open db
-			with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
-			privileges_0 = guilds_config_data[str(guild.id)]["additional-features"]["privileges"][0]  # path to privileges in server
+		with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
+		for guild in guilds_config_data.keys():
+			privileges_0 = guilds_config_data[str(guild)]["additional-features"]["privileges"][0]  # path to privileges in server
 
 			if "premium-time-end" in privileges_0:
 				if datetime.fromisoformat(privileges_0["premium-time-end"]) < datetime.now():
@@ -263,17 +271,17 @@ class Premium(commands.Cog):
 					# write db
 					with open("./.db/multiplayer/guilds.json", "w", encoding="utf-8") as write_file: json.dump(guilds_config_data, write_file, ensure_ascii = False, indent = 4)
 
-					user_dmchannel = self.bot.get_user(guilds_config_data[str(guild.id)]["overview"]["owner-id"])
-					await user_dmchannel.send(f'Премиум подписка на сервере `{guilds_config_data[str(guild.id)]["overview"]["guild_name"]}` закончилась')
+					user_dmchannel = self.bot.get_user(guilds_config_data[str(guild)]["overview"]["owner-id"])
+					await user_dmchannel.send(f'Премиум подписка на сервере `{guilds_config_data[str(guild)]["overview"]["guild-name"]}` закончилась')
 	
 	# запись проверок
 	# обновление окончания времени премиума (на код не влияет, это информационная проверка)
 	@tasks.loop(seconds = bot_tasks_loop_premium_change_premiumtimeremaining())
 	async def premium_change_premiumtimeremaining(self):
-		for guild in self.bot.guilds:
-			# open db
-			with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
-			privileges_0 = guilds_config_data[str(guild.id)]["additional-features"]["privileges"][0]  # path to privileges in server
+		# open db
+		with open("./.db/multiplayer/guilds.json", "r", encoding="utf-8") as read_file: guilds_config_data = json.load(read_file)
+		for guild in guilds_config_data.keys():
+			privileges_0 = guilds_config_data[str(guild)]["additional-features"]["privileges"][0]  # path to privileges in server
 
 			if "premium-time-remaining" in privileges_0:
 				privileges_0["premium-time-remaining"] = f'{str(datetime.fromisoformat(privileges_0["premium-time-end"]) - datetime.now())[:-7]}'
